fd943b8cd3a1e278ad8dcb532ce7a36ce1b906b5
lrn1380 <lrn1380@chmail.ir>
2020-11-05 19:07:55 +0330

    change index.html to multi page by javascript

---
+++ script/tools.js
@@ -0,0 +1,29 @@
+var page_found = 0;
+
+function get_route(_slice){
+	var url = window.location.hash.slice(_slice) || "/";
+  return url;
+}
+function load(url,loc) {
+  $(loc).load(url);
+}
+function _split(str,key,count) {
+  if (count) {
+    var sp = str.split(key,count);
+  }else {
+    var sp = str.split(key);
+   }
+  return sp;
+}
+function route(url,page) {
+  if(get_route(1) == url){
+    load("pages/"+page+".el","#main");
+    //$("#route").attr("src","");
+    page_found=1;
+  }
+}
+function errors(error) {
+  if (page_found == 0) {
+    load("errors/"+error+".error","#main");
+  }
+}

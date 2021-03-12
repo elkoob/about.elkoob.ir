457e421b94ce0ed7a0210ab0bcd29b103e1eb75a
lrn1380 <lrn1380@chmail.ir>
2021-03-13 01:00:37 +0330

    add darkmode

---
+++ script/lightAndDark.js
@@ -0,0 +1,28 @@
+var app = document.getElementById("app")
+var sw = document.getElementById("darkSwitch")
+
+sw.addEventListener("click", checkSwitch)
+
+if (localStorage.getItem("darkMode") == "" || !localStorage.getItem("darkMode")) {
+    localStorage.setItem("darkMode", "yes")
+}
+
+if (localStorage.getItem("darkMode") == "yes"){
+    sw.setAttribute("checked", "checked")
+}
+
+if (localStorage.getItem("darkMode") == "no"){
+    app.setAttribute("class", "light")
+}
+
+
+function checkSwitch() {
+    if (sw.checked == true){
+        localStorage.setItem("darkMode", "yes")
+        app.setAttribute("class", "")
+        app.removeAttribute("class")
+    }else{
+        localStorage.setItem("darkMode", "no")
+        app.setAttribute("class", "light")
+    }
+}

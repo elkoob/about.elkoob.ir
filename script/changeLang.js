27af6ff6c8a8c87afcdf05acb72f483bb36401e8
lrn1380 <lrn1380@chmail.ir>
2021-03-31 15:00:55 +0430

    add load lang pack

--- script/changeLang.js
+++ script/changeLang.js
@@ -18,6 +18,8 @@ if (localStorage.getItem("lang") == "" || !localStorage.getItem("lang")) {
 // app.setAttribute("class", localStorage.getItem("lang"))
 app.classList.add("lang-" + localStorage.getItem("lang"))
 
+loadLangPack(localStorage.getItem("lang"))
+
 for (let index = 0; index < langDiv.childElementCount; index++) {
     // const element = langDiv.children.item(index);
     langDiv.children.item(index).addEventListener("click", () => {setLang(langDiv.children.item(index).getAttribute("lang"))})
@@ -28,5 +30,30 @@ function setLang(lang) {
         app.classList.remove("lang-" + localStorage.getItem("lang"))
         localStorage.setItem("lang", lang)
         app.classList.add("lang-" + lang)
+        loadLangPack(lang)
+    }
+}
+
+function loadLangPack(lang) {
+    var xhr = new XMLHttpRequest()
+    var url = "./lang/" + lang + ".json"
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var resp = JSON.parse(this.responseText)
+            addLangInPage(resp)
+        }
+    };
+    xhr.open("GET", url, true)
+    xhr.send()
+    
+}
+
+function addLangInPage(arr) {
+    var keys = Object.keys(arr)
+    
+    for (let index = 0; index < keys.length; index++) {
+        document.getElementById(keys[index]).innerHTML = arr[keys[index]]
+        
     }
 }

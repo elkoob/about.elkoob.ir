1e218fb59917335460a6b67b18278fd95ee1d9a1
LEDRIF <deldadehmahdi1@gmail.com>
2021-10-03 20:35:53 +0330

    user page added header is not resposive but other section are complete

---
+++ script/script-user.js
@@ -0,0 +1,78 @@
+$(window).scroll(function(){
+    // sticky navbar on scroll script
+    if(this.scrollY > 20){
+        $('.wrapper').addClass("navbar-scroll");
+    }
+    else{
+        $('.wrapper').removeClass("navbar-scroll");
+    }
+});
+
+
+// preloading
+$(document).ready(function() {
+
+  // Fakes the loading setting a timeout
+    setTimeout(function() {
+        $('body').addClass('loaded');
+    }, 3500);
+
+});
+
+//parallax effect
+const parallax = document.getElementById("parallax");
+
+window.addEventListener("scroll" , function () {
+  var offset = window.pageYOffset;
+  const number = -0.2;
+  var inset = offset * number;
+  parallax.style.backgroundPositionY = inset + "px";
+
+});
+
+
+
+//contact team input and textarea animation
+$(".input-effect input").focus(function() {
+  $(this).parent(".input-effect").toggleClass("active");
+});
+
+$(".input-effect input").focusout(function() {
+  $(this).parent(".input-effect").toggleClass("active");
+});
+
+$(".input-effect textarea").focus(function() {
+  $(this).parent(".input-effect").toggleClass("active");
+  $(this).parent(".input-effect").toggleClass("active-textarea");
+});
+
+$(".input-effect textarea").focusout(function() {
+  $(this).parent(".input-effect").toggleClass("active");
+  $(this).parent(".input-effect").toggleClass("active-textarea");
+});
+
+
+//galleries effect
+$(window).on("load", function() {
+  var $container = $(".pics");
+  $container.isotope({
+    filter: '.work' ,
+    animationOptions: {
+      queue: true
+    }
+  });
+  $('.gallery-nav li').click(function() {
+    $('.gallery-nav .current').removeClass('current');
+    $(this).addClass('current');
+    var selector = $(this).attr('data-filter');
+    $container.isotope({
+      filter: selector ,
+      animationOptions: {
+        queue: true
+      }
+    });
+    return false
+  });
+
+  // $('#gallery-item').mixItUp();
+});
